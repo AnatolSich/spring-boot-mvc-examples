d0d035c7cce59192778684e9117c28e15eb7a503
Anatolii Sichkar <anatolii_sichkar@epam.com>
2021-04-17 12:38:00 +0300

    Enabled Asynch processing with DeferredResult return type

--- src/main/java/com/test/hplus/beans/exceptionhadlers/ApplicationExceptionHandler.java
+++ src/main/java/com/test/hplus/beans/exceptionhadlers/ApplicationExceptionHandler.java
@@ -3,11 +3,12 @@ package com.test.hplus.beans.exceptionhadlers;
 import com.test.hplus.exceptions.ApplicationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
-    @ExceptionHandler(ApplicationException.class)
+    @ExceptionHandler({ApplicationException.class, AsyncRequestTimeoutException.class})
     public String handleException() {
         System.out.println("in global exception handler");
         return "error";
